f7fcfcf42944eed0c4fe092cb52aa2b4ebdfa442
pawelwier <pawiercinski@gmail.com>
2023-09-05 23:18:27 +0200

    add sphere, dat.gui and readme

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { WebGLRenderer, Scene, PerspectiveCamera, AxesHelper,
-  BoxGeometry, MeshBasicMaterial, Mesh, PlaneGeometry, GridHelper, DoubleSide,
+  BoxGeometry, MeshBasicMaterial, Mesh, PlaneGeometry, GridHelper, DoubleSide, SphereGeometry,
  } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
 
 const { innerWidth, innerHeight } = window
 
@@ -23,17 +24,25 @@ const camera = new PerspectiveCamera(
 const orbitControls = new OrbitControls(camera, renderer.domElement)
 orbitControls.update()
 
-const axesHelper = new AxesHelper(4)
+const axesHelper = new AxesHelper(20)
 
 scene.add(axesHelper)
 
-camera.position.set(20, 0, 50)
+camera.position.set(5, 5, 50)
 
-const boxGeometry = new BoxGeometry(8, 8, 8)
+const boxGeometry = new BoxGeometry(7, 6, 4)
 const boxMaterial = new MeshBasicMaterial({ color: 0x20FFF1 })
 const box = new Mesh(boxGeometry, boxMaterial)
 
-const planeGeometry = new PlaneGeometry(20, 20)
+const sphereGeometry = new SphereGeometry(3, 30, 30)
+const sphereMaterial = new MeshBasicMaterial({ 
+  color: 0x03123D,
+  wireframe: true
+})
+const sphere = new Mesh(sphereGeometry, sphereMaterial)
+sphere.position.set(10, 6, 0)
+
+const planeGeometry = new PlaneGeometry(30, 30)
 const planeMaterial = new MeshBasicMaterial({ 
   color: 0xFFFFFF,
   side: DoubleSide
@@ -41,9 +50,30 @@ const planeMaterial = new MeshBasicMaterial({
 const plane = new Mesh(planeGeometry, planeMaterial)
 plane.rotation.x = -0.5 * Math.PI
 
-const gridHelper = new GridHelper(20, 20)
+const gui = new dat.GUI()
+
+const options = {
+  sphereColor: '#ff4321',
+  boxColor: '#004333',
+  sphereWireFrame: true
+}
+
+gui
+  .addColor(options, 'sphereColor')
+  .onChange(e => { sphere.material.color.set(e) })
+
+gui
+  .addColor(options, 'boxColor')
+  .onChange(e => { box.material.color.set(e) })
+
+gui
+  .add(options, 'sphereWireFrame')
+  .onChange(e => { sphere.material.wireframe = e })
+
+const gridHelper = new GridHelper(30, 30)
 
 scene.add(box)
+scene.add(sphere)
 scene.add(plane)
 scene.add(gridHelper)
 
